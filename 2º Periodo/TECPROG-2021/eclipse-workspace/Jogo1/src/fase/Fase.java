aa0d5cd75b71b289404f75807981d325f172f46f
Clayton <56970620+srclayton@users.noreply.github.com>
2021-04-20 21:12:52 -0300

    Inicio  do jogo Quack Fire!

---
+++ 2º Periodo/TECPROG-2021/eclipse-workspace/Jogo1/src/fase/Fase.java
@@ -0,0 +1,71 @@
+package fase;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import janela.Janela;
+import player.Player;
+import player.Player2;
+
+public class Fase extends JPanel implements ActionListener{
+	private Image fundo; // background da fase
+	private Player player; // declaro o primeiro player
+	private Player2 player2; // segundo player
+	
+	
+	private Timer timer;	// timer
+	public Fase(){
+		setFocusable(true); // melhora desempenho;
+		setDoubleBuffered(true);
+		ImageIcon img = new ImageIcon("res\\City4.png"); // recebo o src da img;
+		img.setImage(img.getImage().getScaledInstance(Janela.getAlturaJanela(), Janela.getLarguraJanela(), ABORT));
+		this.fundo = img.getImage(); // seto o background cm o src anterior;
+		player = new Player(); // construo o primeiro jogador
+		player.load(); // atualizo o mesmo, imagem e mais detalhes;
+		player2= new Player2();
+		player2.load();
+		addKeyListener(new TecladoAdapter()); // leitura das teclas
+		
+		timer=  new Timer(1, this); // atualiza a cada 1seg
+		timer.start();
+	}
+	
+	public void paint(Graphics g) { // função para mostrar os dados na tela
+		Graphics2D graficos = (Graphics2D) g;
+		graficos.drawImage(fundo, 0, 0, null);
+		graficos.drawImage(player.getImg(), player.getX(), player.getY(), this);
+		graficos.drawImage(player2.getImg(), player2.getX(), player2.getY(), this);
+		g.dispose();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		player.update();
+		player2.update();
+		repaint();
+	}
+	
+	private class TecladoAdapter extends KeyAdapter{
+		@Override
+		public void keyPressed(KeyEvent e) {
+			player.keyPressed(e);
+			player2.keyPressed(e);
+		}
+		@Override
+		public void keyReleased(KeyEvent e) {
+			player.KeyRelease(e);
+			player2.KeyRelease(e);
+		}
+	}
+
+	
+}
